e55760c2ee286caa6db0f610196c76277f55c218
Traunin <traunin5@gmail.com>
2025-11-22 15:48:35 +0300

    feat: db migration

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,7 @@
+DROP TYPE IF EXISTS pr_status;
+
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS team_members;
+DROP TABLE IF EXISTS pull_requests;
+DROP TABLE IF EXISTS reviewers;
